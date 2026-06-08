be17abb796e6f3a61c2b53e6ab78c35b31133d70
Vibhor Sharma <vibhorsharmak@gmail.com>
2026-06-08 17:52:12 +0530

    ci fixes

---
+++ drizzle.config.ci.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/db/schema.js",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    url: process.env.DATABASE_URL || "",
+  },
+});
